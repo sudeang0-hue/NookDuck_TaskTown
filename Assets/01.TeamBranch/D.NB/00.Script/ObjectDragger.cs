658648d7ec3613da56d73f615d5f93d3d1e66f30
nabumi <35137548+nabumi@users.noreply.github.com>
2026-07-08 15:20:34 +0900

    test commit
    테스트 커밋

---
+++ Assets/01.TeamBranch/D.NB/00.Script/ObjectDragger.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class ObjectDragger : MonoBehaviour
+{
+    private Vector3 offset;
+    private bool isDragging = false;
+
+    void Update()
+    {
+        // 마우스 왼쪽 클릭 시
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // Physics.Raycast로 직접 확인 (물리 엔진 강제 호출)
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform == this.transform)
+                {
+                    isDragging = true;
+                    offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(
+                        new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)); // 10은 적절한 거리
+                }
+            }
+        }
+
+        // 드래그 중
+        if (isDragging && Input.GetMouseButton(0))
+        {
+            Vector3 curPos = Camera.main.ScreenToWorldPoint(
+                new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)) + offset;
+            transform.position = curPos;
+        }
+        else
+        {
+            isDragging = false;
+        }
+    }
+}
