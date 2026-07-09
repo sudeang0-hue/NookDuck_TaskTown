6239efc3a466c47e01956ea75f118dce6d9429ac
kay0138 <zeroki0138@gmail.com>
2026-07-09 10:23:56 +0900

    Menu UI 구현
    - 메뉴 버튼 UI 구현 : 보유동물, 보유도구, 뽑기, 도감, 옵션
    - 클릭시 각 메뉴의 패널창 오픈, 목록 버튼으로 닫기

---
+++ Assets/00.Project/01.Scripts/UI/UIController_Menu.cs
@@ -0,0 +1,41 @@
+using KAY;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController_Menu : MonoBehaviour
+{
+    [Header("메뉴 목록 버튼")]
+    [SerializeField] private Button animalInventoryButton;
+    [SerializeField] private Button toolInventoryButton;
+    [SerializeField] private Button gachaButton;
+    [SerializeField] private Button animalDexButton;
+    [SerializeField] private Button optionButton;
+
+    [Header("오픈할 UI 창")]
+    [SerializeField] private UIPanelWindow animalInventoryPanel;
+    [SerializeField] private UIPanelWindow toolInventoryPanel;
+    [SerializeField] private UIPanelWindow gachaPanel;
+    [SerializeField] private UIPanelWindow animalDexPanel;
+    [SerializeField] private UIPanelWindow optionPanel;
+
+
+    private void Awake()
+    {
+        animalInventoryButton.onClick.AddListener(() => TogglePanel(animalInventoryPanel));
+        toolInventoryButton.onClick.AddListener(() => TogglePanel(toolInventoryPanel));
+        gachaButton.onClick.AddListener(() => TogglePanel(gachaPanel));
+        animalDexButton.onClick.AddListener(() => TogglePanel(animalDexPanel));
+        optionButton.onClick.AddListener(() => TogglePanel(optionPanel));
+    }
+
+    private void TogglePanel(UIPanelWindow panel)
+    {
+        if (panel == null)
+        {
+            Debug.LogWarning("[UIController_Menu] 연결되지 않은 패널이 있습니다.");
+            return;
+        }
+
+        panel.TogglePanel();
+    }
+}
