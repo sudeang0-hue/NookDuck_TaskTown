5b1b443ea380a8c7987e4e270ab7b9bd649a65b1
kay0138 <zeroki0138@gmail.com>
2026-07-21 10:10:23 +0900

    축소 화면 UI에 있는 보유 코인 텍스트를 코인 매니저와 연결

--- Assets/00.Project/01.Scripts/UI/UIController_Coin_mini.cs
+++ Assets/00.Project/01.Scripts/UI/UIController_Coin_mini.cs
@@ -1,15 +1,12 @@
 using TMPro;
 using UnityEngine;
 
-public class UIController_Coin : MonoBehaviour
+public class UIController_Coin_mini : MonoBehaviour
 {
+    [SerializeField] private CoinManager coinManager;
 
     [Header("획득 코인 텍스트")]
     [SerializeField] private TMP_Text allCoinText;   // 현재 보유 코인
-    [SerializeField] private TMP_Text autoCoinText;  // 시간당 획득량
-
-    [Header("Text Format")]
-    [SerializeField] private string prefix = " /h";
 
     private bool isSubscribed;
 
@@ -30,9 +27,9 @@ public class UIController_Coin : MonoBehaviour
         if (!isSubscribed)
             return;
 
-        if (CoinManager.Instance != null)
+        if (coinManager != null)
         {
-            CoinManager.Instance.OnCoinChanged -= UpdateAllCoinText;
+            coinManager.OnCoinChanged -= UpdateAllCoinText;
         }
 
         isSubscribed = false;
@@ -43,20 +40,20 @@ public class UIController_Coin : MonoBehaviour
         if (isSubscribed)
             return;
 
-        if (CoinManager.Instance == null)
+        if (coinManager == null)
         {
-            Debug.LogWarning("[UIController_Coin] CoinManager.Instance가 없습니다.");
+            Debug.LogWarning("[UIController_Coin_mini] CoinManager.Instance가 없습니다.");
             return;
         }
 
-        CoinManager.Instance.OnCoinChanged += UpdateAllCoinText;
+        coinManager.OnCoinChanged += UpdateAllCoinText;
         isSubscribed = true;
 
         // 현재 값 즉시 반영
-        UpdateAllCoinText(CoinManager.Instance.totalCoin);
+        UpdateAllCoinText(coinManager.totalCoin);
 
         // 자동 생산은 추후 구현 예정
-        UpdateAutoCoinText(0);
+        //UpdateAutoCoinText(0);
     }
 
 
@@ -80,14 +77,6 @@ public class UIController_Coin : MonoBehaviour
     /// </summary>
     private void UpdateAutoCoinText(long coinPerHour)
     {
-        if (autoCoinText == null)
-        {
-            Debug.LogWarning("[UIController_Coin] autoCoinText가 연결되지 않았습니다.");
-            return;
-        }
-
-        autoCoinText.text = $"{coinPerHour:N0}{prefix}";
+        
     }
-
-
 }
