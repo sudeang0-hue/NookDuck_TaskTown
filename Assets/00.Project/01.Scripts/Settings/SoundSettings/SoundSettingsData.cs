b412c3ad8cd3b13e526986d7cc55c0a89aa7aa39
sudeang0-hue <sudeang0@gmail.com>
2026-07-21 16:43:24 +0900

    [ 26.07.21 - feat ] 사운드 채널 음소거 및 볼륨 복원 기능 구현
    - 채널별 마지막 양수 볼륨 저장 및 기존 데이터 호환 처리
    - 볼륨 아이콘 클릭 시 음소거·이전 볼륨 복원 기능 구현
    - OptionWindowRoot의 채널별 버튼과 SoundSettingsPanel 참조 연결

--- Assets/00.Project/01.Scripts/Settings/SoundSettings/SoundSettingsData.cs
+++ Assets/00.Project/01.Scripts/Settings/SoundSettings/SoundSettingsData.cs
@@ -4,6 +4,7 @@
  *
  * 주요 기능:
  * - Master, BGM, UIController_AnimalInvPage, Environment 볼륨 percent를 보관합니다.
+ * - 채널별 마지막 0 초과 볼륨을 보관해 음소거 해제 시 복원값으로 사용합니다.
  * - 채널별 Get/Set, Clamp, Clone을 제공합니다.
  */
 using System;
@@ -21,10 +22,17 @@ public enum SoundVolumeChannel
 [Serializable]
 public class SoundSettingsData
 {
+    private const float DefaultVolumePercent = 100f;
+
     public float masterVolume = 100f;
     public float bgmVolume = 100f;
     public float uiVolume = 100f;
     [FormerlySerializedAs("animalVolume")] public float environmentVolume = 100f;
+
+    public float masterLastNonZeroVolume;
+    public float bgmLastNonZeroVolume;
+    public float uiLastNonZeroVolume;
+    public float environmentLastNonZeroVolume;
 
     public void Clamp()
     {
@@ -32,6 +40,11 @@ public class SoundSettingsData
         bgmVolume = ClampPercent(bgmVolume);
         uiVolume = ClampPercent(uiVolume);
         environmentVolume = ClampPercent(environmentVolume);
+
+        masterLastNonZeroVolume = NormalizeLastNonZeroVolume(masterLastNonZeroVolume, masterVolume);
+        bgmLastNonZeroVolume = NormalizeLastNonZeroVolume(bgmLastNonZeroVolume, bgmVolume);
+        uiLastNonZeroVolume = NormalizeLastNonZeroVolume(uiLastNonZeroVolume, uiVolume);
+        environmentLastNonZeroVolume = NormalizeLastNonZeroVolume(environmentLastNonZeroVolume, environmentVolume);
     }
 
     public float GetVolume(SoundVolumeChannel channel)
@@ -54,17 +67,33 @@ public class SoundSettingsData
         {
             case SoundVolumeChannel.Master:
                 masterVolume = clampedPercent;
+                UpdateLastNonZeroVolume(ref masterLastNonZeroVolume, clampedPercent);
                 break;
             case SoundVolumeChannel.BGM:
                 bgmVolume = clampedPercent;
+                UpdateLastNonZeroVolume(ref bgmLastNonZeroVolume, clampedPercent);
                 break;
             case SoundVolumeChannel.UI:
                 uiVolume = clampedPercent;
+                UpdateLastNonZeroVolume(ref uiLastNonZeroVolume, clampedPercent);
                 break;
             case SoundVolumeChannel.Environment:
                 environmentVolume = clampedPercent;
+                UpdateLastNonZeroVolume(ref environmentLastNonZeroVolume, clampedPercent);
                 break;
         }
+    }
+
+    public float GetLastNonZeroVolume(SoundVolumeChannel channel)
+    {
+        return channel switch
+        {
+            SoundVolumeChannel.Master => NormalizeLastNonZeroVolume(masterLastNonZeroVolume, masterVolume),
+            SoundVolumeChannel.BGM => NormalizeLastNonZeroVolume(bgmLastNonZeroVolume, bgmVolume),
+            SoundVolumeChannel.UI => NormalizeLastNonZeroVolume(uiLastNonZeroVolume, uiVolume),
+            SoundVolumeChannel.Environment => NormalizeLastNonZeroVolume(environmentLastNonZeroVolume, environmentVolume),
+            _ => DefaultVolumePercent
+        };
     }
 
     public SoundSettingsData Clone()
@@ -74,8 +103,30 @@ public class SoundSettingsData
             masterVolume = masterVolume,
             bgmVolume = bgmVolume,
             uiVolume = uiVolume,
-            environmentVolume = environmentVolume
+            environmentVolume = environmentVolume,
+            masterLastNonZeroVolume = masterLastNonZeroVolume,
+            bgmLastNonZeroVolume = bgmLastNonZeroVolume,
+            uiLastNonZeroVolume = uiLastNonZeroVolume,
+            environmentLastNonZeroVolume = environmentLastNonZeroVolume
         };
+    }
+
+    private static void UpdateLastNonZeroVolume(ref float lastNonZeroVolume, float percent)
+    {
+        if (percent > 0f)
+        {
+            lastNonZeroVolume = percent;
+        }
+    }
+
+    private static float NormalizeLastNonZeroVolume(float lastNonZeroVolume, float currentVolume)
+    {
+        if (lastNonZeroVolume > 0f)
+        {
+            return ClampPercent(lastNonZeroVolume);
+        }
+
+        return currentVolume > 0f ? currentVolume : DefaultVolumePercent;
     }
 
     private static float ClampPercent(float percent)
